c96638cf96c43b7d16a99caa4e28ef0f47b69dfe
MegaOwIer <megaowier@gmail.com>
2023-12-08 18:33:13 +0800

    fix incorrect call of flush()

--- IO/SendSignal.m
+++ IO/SendSignal.m
@@ -1,5 +1,5 @@
 function SendSignal(SigType, OpType)
     str = jsonencode(containers.Map({'sig_type', 'op_type'}, [SigType, OpType]));
     fprintf(1, "%s\n", str);
-    flush(1);
+    drawnow('update');
 end
